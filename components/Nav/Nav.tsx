651485e0a11ff53e089eec5874de3fe144b9b7f1
ivan <ivanlisichkin@icloud.com>
2024-02-08 17:32:33 +0400

    feat:add nav component

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { NextRouter, useRouter } from 'next/router';
+import React from 'react';
+import styles from './Nav.module.scss';
+function Nav() {
+  const router: NextRouter = useRouter();
+  const pathname: string = router.pathname || '';
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <Link href={'/'}>
+          <h1 className={styles.header__title}>BUTIK64</h1>
+        </Link>
+        <nav className={styles.nav}>
+          <Link href={'/stars'}>Звезды</Link>
+          <Link href={'/lifestyle'}>Стиль жизни</Link>
+          <Link href={'/beauty'}>Beauty</Link>
+          <Link href={'/fashion'}>Fashion</Link>
+          <Link href={'/contacts'}>Контакты</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
