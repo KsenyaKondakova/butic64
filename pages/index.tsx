2e1ae91f326cc9ae09ab20ccb19772b87f113361
ivan <ivanlisichkin@icloud.com>
2024-02-12 14:51:16 +0400

    feat:add main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,38 +3,40 @@ import afishaImage from '@/public/assets/images/greyscale-clouds-reflection-in-a
 import newsImage from '@/public/assets/images/old-buildings-and-street-view-in-waitan-of-shanghai-in-black-and-white 1.jpeg';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import Afisha from '@/components/Afisha/Afisha';
 import Banner from '@/components/Banner/Banner';
 import Layout from '@/components/Layout/Layout';
 import MainCard from '@/components/MainCard/MainCard';
 import Nav from '@/components/Nav/Nav';
+import News from '@/components/News/News';
 import Slider from '@/components/Slider/Slider';
+
+import { setPlaces } from '@/redux/slices/placeSlice';
+import { RootState } from '@/redux/store';
 
 import { IPlaceList } from '@/types/placesType';
 
 export default function Home() {
-  const [places, setPlaces] = useState<IPlaceList[]>([]);
-  const [images, setImages] = useState<string[][]>([]);
+  const dispatch = useDispatch();
+
+  const places = useSelector((state: RootState) => state.placeSlice.places);
+  const images = useSelector((state: RootState) => state.placeSlice.images);
+  const news = useSelector((state: RootState) => state.placeSlice.news);
+  const afisha = useSelector((state: RootState) => state.placeSlice.afisha);
   useEffect(() => {
     axios.get('/api/places').then((response) => {
-      setPlaces(response.data);
+      dispatch(setPlaces(response.data));
     });
   }, []);
-  console.log(places);
-  const getImagesPlaces = () => {
-    setImages(places.map((place) => place.images || [])); // flat() используется для устранения вложенности
-  };
-  console.log('картинки', images);
-  useEffect(() => {
-    // Вызывать getImagesPlaces только если places изменился
-    getImagesPlaces();
-  }, [places]);
+  console.log(news);
   return (
     <>
       <Nav />
       <Layout>
         <div className="main__page__container">
-          <MainCard
+          {/* <MainCard
             title={'Новости города'}
             text={'Здесь вы найдете последние новости города'}
             image={newsImage}
@@ -54,12 +56,17 @@ export default function Home() {
             image={galleryImage}
             gridClass="gallery"
             link="/gallery"
-          />
-          {/* <div className="gallery">
-            {images.map((imageList, index) => (
-              <Slider key={index} images={imageList} sliderIndex={index} />
-            ))}
-          </div> */}
+          /> */}
+          <News news={news} />
+          <div className="gallery">
+            {images.map(
+              (imageList, index) =>
+                imageList.length !== 0 && (
+                  <Slider key={index} images={imageList} sliderIndex={index} />
+                ),
+            )}
+          </div>
+          <Afisha afisha={afisha} />
         </div>
         <Banner />
       </Layout>
