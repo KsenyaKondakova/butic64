94660bd7e178295439cd833b690aee961e19c74c
ivan <ivanlisichkin@icloud.com>
2024-02-12 14:50:11 +0400

    fix:change view slider

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
-import styles from './Slider.module.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+import styles from './Slider.module.scss';
+
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import ChevronLeft from '@/public/assets/icons/ChevronLeft';
 import ChevronRight from '@/public/assets/icons/ChevronRight';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
 interface SliderProps {
   images: string[];
   sliderIndex: number;
@@ -18,13 +21,28 @@ const Slider: React.FC<SliderProps> = ({ images, sliderIndex }) => {
     <div className={styles.container}>
       <Swiper
         modules={[Navigation]}
-        spaceBetween={50}
-        slidesPerView={3}
         navigation={{
           nextEl: '.button-next',
           prevEl: '.button-prev',
         }}
-        className={'mySwiper ' + sliderIndex}>
+        className={'mySwiper ' + sliderIndex}
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+
+          1440: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        }}
+      >
         <button className={`button-prev ${styles.button}`}>
           <ChevronLeft />
         </button>
