3290811c18db95582f38d97de5f8fcc1a64f156f
ivan <ivanlisichkin@icloud.com>
2024-02-13 13:26:38 +0400

    feat:add fashion page

---
+++ redux/slices/fashionSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { IPlacePageState } from '@/types/placesType';
+
+import { IPlaceList } from '../../types/placesType';
+
+const initialState: IPlacePageState = {
+  places: [],
+  placeInfo: { _id: null, title: '', description: '', category: '', news: [] },
+};
+
+export const fashionSlice = createSlice({
+  name: 'beauty',
+  initialState,
+  reducers: {
+    setFashionPlaces: (state, action: PayloadAction<IPlaceList[]>) => {
+      state.places = action.payload;
+    },
+    setFashionPlaceInfo: (state, action: PayloadAction<IPlaceList>) => {
+      state.placeInfo = action.payload;
+    },
+  },
+});
+
+export const { setFashionPlaces, setFashionPlaceInfo } = fashionSlice.actions;
+export default fashionSlice.reducer;
