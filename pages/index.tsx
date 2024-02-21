3ed10a9c9d68d853fdb534e7dfd5521db32d0efc
ivan <ivanlisichkin@icloud.com>
2024-02-21 19:20:19 +0400

    feat:add date for slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,60 @@
-import galleryImage from '@/public/assets/images/abstract-background-border-black-smoke-texture-border-cinematic-design 1.jpeg';
-import afishaImage from '@/public/assets/images/greyscale-clouds-reflection-in-a-building 1.jpeg';
-import newsImage from '@/public/assets/images/old-buildings-and-street-view-in-waitan-of-shanghai-in-black-and-white 1.jpeg';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Afisha from '@/components/Afisha/Afisha';
 import Banner from '@/components/Banner/Banner';
 import Layout from '@/components/Layout/Layout';
-import MainCard from '@/components/MainCard/MainCard';
 import Nav from '@/components/Nav/Nav';
 import News from '@/components/News/News';
 import Slider from '@/components/Slider/Slider';
 
+import { setAfisha, setMergeAfisha } from '@/redux/slices/afishaSlice';
+import { setMergeNews, setNews } from '@/redux/slices/newsSlice';
 import { setPlaces } from '@/redux/slices/placeSlice';
 import { RootState } from '@/redux/store';
-
-import { IPlaceList } from '@/types/placesType';
 
 export default function Home() {
   const dispatch = useDispatch();
 
   const places = useSelector((state: RootState) => state.placeSlice.places);
-  const images = useSelector((state: RootState) => state.placeSlice.images);
-  const news = useSelector((state: RootState) => state.placeSlice.news);
-  const afisha = useSelector((state: RootState) => state.placeSlice.afisha);
+  const placesNews = useSelector((state: RootState) => state.placeSlice.news);
+  const placesAfisha = useSelector(
+    (state: RootState) => state.placeSlice.afisha,
+  );
+  const mainNews = useSelector((state: RootState) => state.newsSlice.news);
+  const mainAfisha = useSelector(
+    (state: RootState) => state.afishaSlice.afisha,
+  );
+  const mergeNews = useSelector(
+    (state: RootState) => state.newsSlice.mergeNews,
+  );
+  const mergeAfisha = useSelector(
+    (state: RootState) => state.afishaSlice.mergeAfisha,
+  );
   useEffect(() => {
     axios.get('/api/places').then((response) => {
       dispatch(setPlaces(response.data));
     });
   }, []);
-  console.log(news);
+  useEffect(() => {
+    axios.get('/api/news').then((response) => {
+      dispatch(setNews(response.data));
+    });
+  }, []);
+  useEffect(() => {
+    axios.get('/api/afisha').then((response) => {
+      dispatch(setAfisha(response.data));
+    });
+  }, []);
+  useEffect(() => {
+    dispatch(setMergeNews({ placesNews, mainNews }));
+  }, [placesNews, mainNews]);
+  useEffect(() => {
+    dispatch(setMergeAfisha({ placesAfisha, mainAfisha }));
+  }, [placesAfisha, mainAfisha]);
+  console.log(mergeNews);
   return (
     <>
       <Nav />
@@ -57,16 +81,29 @@ export default function Home() {
             gridClass="gallery"
             link="/gallery"
           /> */}
-          <News news={news} />
+          <News news={mergeNews} title={'Новости города'} />
           <div className="gallery">
-            {images.map(
-              (imageList, index) =>
-                imageList.length !== 0 && (
-                  <Slider key={index} images={imageList} sliderIndex={index} />
-                ),
-            )}
+            {places.map((place, index) => {
+              if (place.images && place.images.length > 0) {
+                return (
+                  <div key={place._id} className="gallery__container">
+                    <Slider
+                      key={index}
+                      images={place.images}
+                      sliderIndex={index}
+                    />
+                    <div className="gallery__container__text">
+                      <Link href={`/place/${place._id}`}>{place.title}</Link>
+                      <span>{place.dateImages}</span>
+                    </div>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })}
           </div>
-          <Afisha afisha={afisha} />
+          <Afisha afisha={mergeAfisha} title={'Афиша города'} />
         </div>
         <Banner />
       </Layout>
