bce3ef84d9a9b88bfcf2cd7f5a19c6abaca01f9e
ivan <ivanlisichkin@icloud.com>
2024-02-21 19:19:12 +0400

    feat:add main afisha and link for item

--- components/Afisha/Afisha.tsx
+++ components/Afisha/Afisha.tsx
@@ -1,19 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import { AfishaProps } from '@/types/placesType';
 
 import styles from './Afisha.module.scss';
 
-const Afisha: React.FC<AfishaProps> = ({ afisha }) => {
+const Afisha: React.FC<AfishaProps> = ({ afisha, title }) => {
   return (
     <div className={styles.afisha}>
-      {afisha.map((image, index) => (
-        <div key={index} className={styles.container}>
-          <img src={image} alt="" className={styles.image} />
-        </div>
-      ))}
+      <h3 className={styles.title}>{title}</h3>
+      {afisha.map((item, index) =>
+        item.parent ? (
+          <Link
+            href={`/place/${item.parent}`}
+            key={index}
+            className={styles.news__container}
+          >
+            <Image
+              src={item.image}
+              alt=""
+              layout="responsive"
+              objectFit="contain"
+              width={100}
+              height={100}
+            />
+          </Link>
+        ) : (
+          <div key={index} className={styles.container}>
+            <Image
+              src={item.image}
+              alt=""
+              layout="responsive"
+              objectFit="contain"
+              width={100}
+              height={100}
+            />
+          </div>
+        ),
+      )}
     </div>
   );
 };
 
 export default Afisha;
+//<img src={image} alt="" className={styles.image} />
