5132b6535055e9959a09a2f5737c82450a685874
ivan <ivanlisichkin@icloud.com>
2024-02-26 23:29:01 +0400

    feat:add mobile layout

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Nav.module.scss';
 
 function Nav() {
   const router: NextRouter = useRouter();
   const pathname: string = router.pathname || '';
-
+  const [menuActive, setMenuActive] = useState<boolean>(false);
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
@@ -21,6 +21,35 @@ function Nav() {
           <Link href={'/fashion'}>Fashion</Link>
           <Link href={'/contacts'}>Контакты</Link>
         </nav>
+      </div>
+      <div className={styles.header__container__mobile}>
+        <div
+          className={styles.menu__mobile__btn__container}
+          onClick={() => setMenuActive(!menuActive)}
+        >
+          <div className={styles.menu__mobile__btn}>
+            <span></span>
+          </div>
+        </div>
+
+        <Link href={'/'} className={styles.header__title__mobile__link}>
+          <h1 className={styles.header__title__mobile}>БУТИК64</h1>
+        </Link>
+        <div
+          className={`${styles.menu__mobile} ${menuActive ? styles.active : ''}`}
+        >
+          <div
+            className={styles.menu__mobile__blur}
+            onClick={() => setMenuActive(false)}
+          ></div>
+          <nav className={styles.nav__mobile}>
+            <Link href={'/stars'}>Звезды</Link>
+            <Link href={'/lifestyle'}>Стиль жизни</Link>
+            <Link href={'/beauty'}>Beauty</Link>
+            <Link href={'/fashion'}>Fashion</Link>
+            <Link href={'/contacts'}>Контакты</Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
