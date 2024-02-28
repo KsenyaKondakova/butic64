da9bff976d0d25437f0d13b57aaf6fdf9384263c
ivan <ivanlisichkin@icloud.com>
2024-02-28 19:21:45 +0400

    feat:add contacts page

---
+++ pages/contacts.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Footer } from '@/components/Footer/Footer';
+import Layout from '@/components/Layout/Layout';
+import Nav from '@/components/Nav/Nav';
+
+function Contacts() {
+  return (
+    <>
+      <Nav />
+      <Layout>
+        <div className="contacts">
+          <span className="contacts__title">Отправьте нам сообщение</span>
+          <p className="contacts__text">
+            Если у вас есть какие-то вопросы или предложение по сотрудничеству -
+            напишите на почту &nbsp;
+            <a href="mailto:secular@butic64.ru">secular@butic64.ru</a>
+          </p>
+        </div>
+        <Footer />
+      </Layout>
+    </>
+  );
+}
+
+export default Contacts;
