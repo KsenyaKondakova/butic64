9f07faf078ab53465e7f6160704644ec39dce06b
ivan <ivanlisichkin@icloud.com>
2024-03-08 18:07:44 +0400

    feat:add showdown menu

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,13 +1,33 @@
+import {
+  useBeautyCategoriesFetch,
+  useFashionCategoriesFetch,
+  useLyfeStyleCategoriesFetch,
+} from '@/hooks/useDataFetching';
+import {
+  useBeautyCategories,
+  useFashionCategories,
+  useLifeStyleCategories,
+  usePlacesData,
+} from '@/hooks/useReduxSelectors';
+import axios from 'axios';
 import Link from 'next/link';
 import { NextRouter, useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './Nav.module.scss';
 
 function Nav() {
   const router: NextRouter = useRouter();
+  const places = usePlacesData();
   const pathname: string = router.pathname || '';
   const [menuActive, setMenuActive] = useState<boolean>(false);
+  useLyfeStyleCategoriesFetch();
+  useFashionCategoriesFetch();
+  useBeautyCategoriesFetch();
+  const lifeStyleCategories = useLifeStyleCategories();
+  const beautyCategories = useBeautyCategories();
+  const fashionCategories = useFashionCategories();
+
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
@@ -15,11 +35,79 @@ function Nav() {
           <h1 className={styles.header__title}>БУТИК64</h1>
         </Link>
         <nav className={styles.nav}>
-          <Link href={'/stars'}>Звезды</Link>
-          <Link href={'/lifestyle'}>Стиль жизни</Link>
-          <Link href={'/beauty'}>Beauty</Link>
-          <Link href={'/fashion'}>Fashion</Link>
-          <Link href={'/contacts'}>Контакты</Link>
+          <ul className={styles.topmenu}>
+            <li>
+              <span>Звезды</span>
+              <ul className={styles.submenu}>
+                <li>
+                  <Link href={'/stars'}>Все звезды</Link>
+                </li>
+                <li>
+                  <a href="">Author</a>
+                </li>
+                <li>
+                  <a href="">Archive</a>
+                </li>
+                <li>
+                  <a href="">Tags</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <span>Стиль жизни</span>
+              <ul className={styles.submenu}>
+                <li>
+                  <Link href={'/lifestyle/'}>Все заведения</Link>
+                </li>
+                {lifeStyleCategories.map((item, index) => {
+                  return (
+                    places.find((obj) => obj.category === item._id) && (
+                      <li key={index}>
+                        <Link href={'/lifestyle/' + item._id}>{item.name}</Link>
+                      </li>
+                    )
+                  );
+                })}
+              </ul>
+            </li>
+            <li>
+              <span className={styles.down}>Beauty</span>
+              <ul className={styles.submenu}>
+                <li>
+                  <Link href={'/beauty/'}>Все заведения</Link>
+                </li>
+                {beautyCategories.map((item, index) => {
+                  return (
+                    places.find((obj) => obj.category === item._id) && (
+                      <li key={index}>
+                        <Link href={'/beauty/' + item._id}>{item.name}</Link>
+                      </li>
+                    )
+                  );
+                })}
+              </ul>
+            </li>
+            <li>
+              <span className={styles.down}>Fashion</span>
+              <ul className={styles.submenu}>
+                <li>
+                  <Link href={'/fashion/'}>Все заведения</Link>
+                </li>
+                {fashionCategories.map((item, index) => {
+                  return (
+                    places.find((obj) => obj.category === item._id) && (
+                      <li key={index}>
+                        <Link href={'/fashion' + item._id}>{item.name}</Link>
+                      </li>
+                    )
+                  );
+                })}
+              </ul>
+            </li>
+            <li>
+              <span>Контакты</span>
+            </li>
+          </ul>
         </nav>
       </div>
       <div className={styles.header__container__mobile}>
---
+++ pages/beauty/[...id].tsx
@@ -0,0 +1,114 @@
+import {
+  useAfishaFetch,
+  useLifeStyleFetch,
+  useNewsFetch,
+  usePlaceFetch,
+} from '@/hooks/useDataFetching';
+import {
+  useBeautyPlacesData,
+  useLifeStyleCategories,
+  useMainAfisha,
+  useMainNews,
+  useMergeAfisha,
+  useMergeNews,
+  useModalAfisha,
+  useModalNews,
+  usePlacesAfisha,
+  usePlacesNews,
+} from '@/hooks/useReduxSelectors';
+import { convertISOToCustomFormat } from '@/utils/date';
+import axios from 'axios';
+import { NextRouter, useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import Afisha from '@/components/Afisha/Afisha';
+import Cards from '@/components/Cards/Cards';
+import Layout from '@/components/Layout/Layout';
+import { Modal } from '@/components/Modal/Modal';
+import News from '@/components/News/News';
+
+import { setMergeAfisha } from '@/redux/slices/afishaSlice';
+import { setBeautyPlaces } from '@/redux/slices/beautySlice';
+import { setMergeNews } from '@/redux/slices/newsSlice';
+
+function Lifestyle() {
+  const router: NextRouter = useRouter();
+  const id: string | string[] | undefined = router.query.id;
+  const dispatch = useDispatch();
+  const places = useBeautyPlacesData();
+  const categories = useLifeStyleCategories();
+  const placesNews = usePlacesNews();
+  const placesAfisha = usePlacesAfisha();
+  const mainNews = useMainNews();
+  const mainAfisha = useMainAfisha();
+  const mergeNews = useMergeNews();
+  const mergeAfisha = useMergeAfisha();
+  const modalNews = useModalNews();
+  const modalAfisha = useModalAfisha();
+  const [modalActive, setModalActive] = useState<boolean>(false);
+  const [modalNewsOrImage, setModalNewsOrImage] = useState<boolean>(false);
+  usePlaceFetch();
+  useNewsFetch();
+  useAfishaFetch();
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    console.log(id);
+    const categoryId = Array.isArray(id) ? id[0] : id;
+    axios.get('/api/beauty?id=' + categoryId).then((response) => {
+      dispatch(setBeautyPlaces(response.data));
+    });
+  }, [id]);
+
+  useEffect(() => {
+    dispatch(setMergeNews({ placesNews, mainNews }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [placesNews, mainNews]);
+  useEffect(() => {
+    dispatch(setMergeAfisha({ placesAfisha, mainAfisha }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [placesAfisha, mainAfisha]);
+  return (
+    <Layout>
+      <News
+        news={mergeNews}
+        title={'Новости города'}
+        setModalActive={setModalActive}
+        setModalNewsOrImage={setModalNewsOrImage}
+      />
+      <Cards data={places} categories={categories} />
+      <Afisha
+        afisha={mergeAfisha}
+        title={'Афиша города'}
+        setModalActive={setModalActive}
+        setModalNewsOrImage={setModalNewsOrImage}
+      />
+      <Modal modalActive={modalActive} setModalActive={setModalActive}>
+        {modalNewsOrImage ? (
+          <>
+            <span className="modal__newsName">{modalNews.newsName}</span>
+            <p
+              className="modal__newsText"
+              dangerouslySetInnerHTML={{
+                __html: modalNews.newsText
+                  .split('\n')
+                  .map((line) => `<span>${line}</span>`)
+                  .join('<br>'),
+              }}
+            />
+            <span className="modal__newsDate">
+              {convertISOToCustomFormat(modalNews.date)}
+            </span>
+          </>
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img className="modal__afisha" src={modalAfisha.image} alt="f" />
+        )}
+      </Modal>
+    </Layout>
+  );
+}
+
+export default Lifestyle;
