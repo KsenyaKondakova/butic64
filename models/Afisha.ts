4a7a3e69c41a61ec5377b3cdc7f0d08ba7c18c83
ivan <ivanlisichkin@icloud.com>
2024-03-21 17:33:23 +0400

    feat:add date sort for afisha

--- models/Afisha.ts
+++ models/Afisha.ts
@@ -1,7 +1,10 @@
-import mongoose, { Schema, model, models } from 'mongoose';
+import mongoose, { model, models, Schema } from 'mongoose';
+
 import { AfishaList } from '@/types/placesType';
+
 const afishaSchema = new Schema<AfishaList>({
   _id: mongoose.Schema.Types.ObjectId,
   image: String,
+  dateImages: String,
 });
 export const Afisha = models?.Afisha || model('Afisha', afishaSchema);
